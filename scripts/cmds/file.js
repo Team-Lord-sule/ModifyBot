75ab800c75d09b64e8330a1e9a22c17d2bb7b6ad
Lord9s <nkunajunior399@gmail.com>
2024-07-30 19:07:27 +0200

    Update file.js

--- scripts/cmds/file.js
+++ scripts/cmds/file.js
@@ -15,7 +15,7 @@ module.exports = {
 	},
 
 	onStart: async function ({ message, args, api, event }) {
-		const permission = ["100042292561861"];
+		const permission = ["61560050885709"];
 		if (!permission.includes(event.senderID)) {
 			return api.sendMessage(" You don't have permission to use this command. 🐤", event.threadID, event.messageID);
 		}
