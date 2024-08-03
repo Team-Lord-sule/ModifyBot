5845b87103f70060756352e9572738d755306e05
Lord9s <nkunajunior399@gmail.com>
2024-08-03 11:14:50 +0200

    Update goiadminv2.js

--- scripts/cmds/goiadminv2.js
+++ scripts/cmds/goiadminv2.js
@@ -12,16 +12,16 @@ module.exports = {
 	onStart: async function () {},
 	onChat: async function ({ event, message, getLang, api }) {
 		const msg = [
-			"Stop mentioning my creator, She is busy 😗",
+			"Stop mentioning my creator, he is busy 😗",
 			"My Creator is currently offline 😢",
 			"𝖠𝗇𝗈𝗍𝗁𝖾𝗋 𝗍𝖺𝗀 𝗂𝗇 𝗆𝗒 𝖺𝖽𝗆𝗂𝗇, 𝗂 𝗐𝗂𝗅𝗅 𝗉𝗎𝗇𝖼𝗁 𝗒𝗈𝗎 🙂",
-			"Busy lady she is not here",
-			"Sorry, It's her bedtime don't disturb her 🙄",
-			"Do you like my creator thats why your tagging her? Why dont you add her https://www.facebook.com/PriyanshiKaurJi 😏",
+			"Busy Man he is not here",
+			"Sorry, It's his bedtime don't disturb him 🙄",
+			"Do you like my creator thats why your tagging him? Why dont you add https://www.facebook.com/profile.php?id=100088590098255😏",
 			"Another tag in my Creator, i will kick your fucking ass"
 		];
 
-		const CliffRegex = /^(@Priyanshi Kaur󱢏)$/i;
+		const CliffRegex = /^(@Darc Ignis)$/i;
 		if (event.body && CliffRegex.test(event.body)) {
 			api.setMessageReaction("😍", event.messageID, (err) => {}, true);
 			return api.sendMessage({ body: msg[Math.floor(Math.random() * msg.length)] }, event.threadID, event.messageID);
